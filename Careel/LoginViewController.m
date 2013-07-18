a9ece2a5a9b791571f510fef03d997e7e025ed0c
ryonext <ryonext.s@gmail.com>
2013-07-18 20:33:53 +0900

    add TwitterSignedRequest

--- Careel/LoginViewController.m
+++ Careel/LoginViewController.m
@@ -9,8 +9,24 @@
 #import "LoginViewController.h"
 #import <Social/Social.h>
 #import <Accounts/Accounts.h>
+#import "OAuth+Additions.h"
+#import "TWAPIManager.h"
+#import "TWSignedRequest.h"
+
+#define ERROR_TITLE_MSG @"Whoa, there cowboy"
+#define ERROR_NO_ACCOUNTS @"You must add a Twitter account in Settings.app to use this demo."
+#define ERROR_PERM_ACCESS @"We weren't granted access to the user's accounts"
+#define ERROR_NO_KEYS @"You need to add your Twitter app keys to Info.plist to use this demo.\nPlease see README.md for more info."
+#define ERROR_OK @"OK"
+
 
 @interface LoginViewController ()
+
+@property (nonatomic, strong) ACAccountStore *accountStore;
+@property (nonatomic, strong) TWAPIManager *apiManager;
+@property (nonatomic, strong) NSArray *accounts;
+@property (nonatomic, strong) UIButton *reverseAuthBtn;
+
 
 @end
 
@@ -39,8 +55,13 @@
 
 - (IBAction)loginWithTwitter:(id)sender {
     NSLog(@"press");
-    ACAccountStore *accountStore = [[ACAccountStore alloc] init];
-    ACAccountType *accountType = [accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    
+    // Twitterアカウントを取得する
+    
+    // Ca:REELサーバーにPOSTする
+    
+//    ACAccountStore *accountStore = [[ACAccountStore alloc] init];
+//    ACAccountType *accountType = [accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
 //    [accountStore requestAccessToAccountsWithType:accountType options:nil completion:<#^(BOOL granted, NSError *error)completion#>]{
 //        if(granted){
 //            NSArray *accountArray = [accountStore accountWithAccountType:accountType];
@@ -49,7 +70,67 @@
 //            }
 //        }
 //    }
-    
-    
 }
+
+/**
+ *  Checks for the current Twitter configuration on the device / simulator.
+ *
+ *  First, we check to make sure that we've got keys to work with inside Info.plist (see README)
+ *
+ *  Then we check to see if the device has accounts available via +[TWAPIManager isLocalTwitterAccountAvailable].
+ *
+ *  Next, we ask the user for permission to access his/her accounts.
+ *
+ *  Upon completion, the button to continue will be displayed, or the user will be presented with a status message.
+ */
+- (void)refreshTwitterAccounts
+{
+    // TWDLog(@"Refreshing Twitter Accounts \n");
+    
+    if (![TWAPIManager hasAppKeys]) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:ERROR_TITLE_MSG message:ERROR_NO_KEYS delegate:nil cancelButtonTitle:ERROR_OK otherButtonTitles:nil];
+        [alert show];
+    }
+    else if (![TWAPIManager isLocalTwitterAccountAvailable]) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:ERROR_TITLE_MSG message:ERROR_NO_ACCOUNTS delegate:nil cancelButtonTitle:ERROR_OK otherButtonTitles:nil];
+        [alert show];
+    }
+    else {
+        [self obtainAccessToAccountsWithBlock:^(BOOL granted) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (granted) {
+//                    _reverseAuthBtn.enabled = YES;
+                }
+                else {
+                    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:ERROR_TITLE_MSG message:ERROR_PERM_ACCESS delegate:nil cancelButtonTitle:ERROR_OK otherButtonTitles:nil];
+                    [alert show];
+  //                  TWALog(@"You were not granted access to the Twitter accounts.");
+                }
+            });
+        }];
+    }
+}
+
+
+- (void)obtainAccessToAccountsWithBlock:(void (^)(BOOL))block
+{
+    ACAccountType *twitterType = [_accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    
+    ACAccountStoreRequestAccessCompletionHandler handler = ^(BOOL granted, NSError *error) {
+        if (granted) {
+            self.accounts = [_accountStore accountsWithAccountType:twitterType];
+        }
+        
+        block(granted);
+    };
+    
+    //  This method changed in iOS6. If the new version isn't available, fall back to the original (which means that we're running on iOS5+).
+    if ([_accountStore respondsToSelector:@selector(requestAccessToAccountsWithType:options:completion:)]) {
+        [_accountStore requestAccessToAccountsWithType:twitterType options:nil completion:handler];
+    }
+    else {
+        [_accountStore requestAccessToAccountsWithType:twitterType withCompletionHandler:handler];
+    }
+}
+
 @end
